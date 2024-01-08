dc8df3ecd76f9fa5573dd0478d22669f8f3af05d
User <User@DESKTOP-QPE9SBA>
2024-01-08 09:17:11 +0800

    Updated Test Cases

---
+++ Object Repository/Page_Watsons User/tooltip_TicketType.rs
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>tooltip_TicketType</name>
+   <tag></tag>
+   <elementGuidId>78a39918-fcdc-48ef-b96c-91a9de177bc6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>CSS</key>
+         <value>i.icon.eyeicon.fa.fa-info-circle.fa-1x</value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@id='l5_0-74_0-b7-b1-Content']/i</value>
+      </entry>
+      <entry>
+         <key>BASIC</key>
+         <value>id(&quot;l5_0-74_0-b7-b1-Content&quot;)/i[@class=&quot;icon eyeicon fa fa-info-circle fa-1x&quot;][count(. | //i[@class = 'icon eyeicon fa fa-info-circle fa-1x']) = count(//i[@class = 'icon eyeicon fa fa-info-circle fa-1x'])]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>BASIC</selectorMethod>
+   <useRalativeImagePath>true</useRalativeImagePath>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>tag</name>
+      <type>Main</type>
+      <value>i</value>
+      <webElementGuid>a70875ba-215e-49b3-9993-940671f63492</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>class</name>
+      <type>Main</type>
+      <value>icon eyeicon fa fa-info-circle fa-1x</value>
+      <webElementGuid>0f19a8c8-613e-4339-b39e-2344d49e9a76</webElementGuid>
+   </webElementProperties>
+   <webElementProperties>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath</name>
+      <type>Main</type>
+      <value>id(&quot;l5_0-74_0-b7-b1-Content&quot;)/i[@class=&quot;icon eyeicon fa fa-info-circle fa-1x&quot;]</value>
+      <webElementGuid>5934dfc2-f827-4e65-a2e9-6b59cc17e57a</webElementGuid>
+   </webElementProperties>
+   <webElementXpaths>
+      <isSelected>true</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:idRelative</name>
+      <type>Main</type>
+      <value>//div[@id='l5_0-74_0-b7-b1-Content']/i</value>
+      <webElementGuid>722f851e-6f71-4405-a6d2-4a8bfdbed376</webElementGuid>
+   </webElementXpaths>
+   <webElementXpaths>
+      <isSelected>false</isSelected>
+      <matchCondition>equals</matchCondition>
+      <name>xpath:position</name>
+      <type>Main</type>
+      <value>//div/i</value>
+      <webElementGuid>6b760251-6037-4e78-a4f4-1601010b23da</webElementGuid>
+   </webElementXpaths>
+</WebElementEntity>
